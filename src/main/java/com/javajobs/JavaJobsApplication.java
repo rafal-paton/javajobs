20572992970345aec5de1e09feb40bb96ee92886
Rafał Patoń <rafal.paton@gmail.com>
2024-11-20 11:56:20 +0100

    project created, spring boot added

---
+++ src/main/java/com/javajobs/JavaJobsApplication.java
@@ -0,0 +1,13 @@
+package com.javajobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javajobs/JavaJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
